0e88ddc7f7fd541b4ae46fb16b686b535b838c37
Rinaldes Duma <rinaldes.duma@gmail.com>
2024-06-05 16:28:56 +0700

    refactor: :recycle: revamp table column

--- invoice_schema.sql
+++ invoice_schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE invoice."transaction" (
 	"created_at" TIMESTAMPTZ DEFAULT current_timestamp,
 	"updated_at" TIMESTAMPTZ DEFAULT current_timestamp,
 	"deleted_at" TIMESTAMPTZ,
-	"is_deleted" boolean,
+	
 	"total_transaction" float NOT NULL,
 	PRIMARY KEY("id")
 );
@@ -28,7 +28,7 @@ CREATE TABLE invoice."seller" (
 	"created_at" TIMESTAMPTZ DEFAULT current_timestamp,
 	"updated_at" TIMESTAMPTZ DEFAULT current_timestamp,
 	"deleted_at" TIMESTAMPTZ,
-	"is_deleted" boolean,
+	
 	PRIMARY KEY("id")
 );
 
@@ -39,7 +39,7 @@ CREATE TABLE invoice."user" (
 	"created_at" TIMESTAMPTZ DEFAULT current_timestamp,
 	"updated_at" TIMESTAMPTZ DEFAULT current_timestamp,
 	"deleted_at" TIMESTAMPTZ,
-	"is_deleted" boolean,
+	
 	PRIMARY KEY("id")
 );
 
@@ -47,7 +47,7 @@ CREATE TABLE invoice."shipper" (
 	"id" serial NOT NULL,
 	"name" text,
 	"shipping_fee" float,
-	"is_deleted" boolean,
+	
 	PRIMARY KEY("id")
 );
 
@@ -60,7 +60,7 @@ CREATE TABLE invoice."transaction_product" (
 	"created_at" TIMESTAMPTZ DEFAULT current_timestamp,
 	"updated_at" TIMESTAMPTZ DEFAULT current_timestamp,
 	"deleted_at" TIMESTAMPTZ,
-	"is_deleted" boolean,
+	
 	PRIMARY KEY("id")
 );
 
@@ -71,7 +71,7 @@ CREATE TABLE invoice."product" (
 	"created_at" TIMESTAMPTZ DEFAULT current_timestamp,
 	"updated_at" TIMESTAMPTZ DEFAULT current_timestamp,
 	"deleted_at" TIMESTAMPTZ,
-	"is_deleted" boolean,
+	
 	PRIMARY KEY("id")
 );
 
@@ -79,10 +79,11 @@ CREATE TABLE invoice."transaction_payment" (
 	"id" serial NOT NULL,
 	"transaction_id" int,
 	"payment_id" int,
+	"amount" float DEFAULT 0,
 	"created_at" TIMESTAMPTZ DEFAULT current_timestamp,
 	"updated_at" TIMESTAMPTZ DEFAULT current_timestamp,
 	"deleted_at" TIMESTAMPTZ,
-	"is_deleted" boolean,
+	
 	PRIMARY KEY("id")
 );
 
@@ -93,7 +94,7 @@ CREATE TABLE invoice."payment" (
 	"created_at" TIMESTAMPTZ DEFAULT current_timestamp,
 	"updated_at" TIMESTAMPTZ DEFAULT current_timestamp,
 	"deleted_at" TIMESTAMPTZ,
-	"is_deleted" boolean,
+	
 	PRIMARY KEY("id")
 );
 
@@ -103,7 +104,7 @@ CREATE TABLE invoice."payment_type" (
 	"created_at" TIMESTAMPTZ DEFAULT current_timestamp,
 	"updated_at" TIMESTAMPTZ DEFAULT current_timestamp,
 	"deleted_at" TIMESTAMPTZ,
-	"is_deleted" boolean,
+	
 	PRIMARY KEY("id")
 );
 
@@ -130,10 +131,13 @@ CREATE TABLE invoice."promo" (
 	"amount" float,
 	"amount_type" invoice.amount_type_t,
 	"requirement_id" int,
+	"quota" int DEFAULT 0,
+	"start_date" TIMESTAMPTZ DEFAULT current_timestamp,
+	"end_date" TIMESTAMPTZ DEFAULT current_timestamp,
 	"created_at" TIMESTAMPTZ DEFAULT current_timestamp,
 	"updated_at" TIMESTAMPTZ DEFAULT current_timestamp,
 	"deleted_at" TIMESTAMPTZ,
-	"is_deleted" boolean,
+	
 	PRIMARY KEY("id")
 );
 
@@ -143,9 +147,21 @@ CREATE TABLE invoice."promo_requirement" (
 	"created_at" TIMESTAMPTZ DEFAULT current_timestamp,
 	"updated_at" TIMESTAMPTZ DEFAULT current_timestamp,
 	"deleted_at" TIMESTAMPTZ,
-	"is_deleted" boolean,
+	
 	PRIMARY KEY("id")
 );
+
+CREATE TABLE
+  invoice.redeemed_promo (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id BIGINT NOT NULL,
+    promo_id BIGINT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    FOREIGN KEY (user_id) REFERENCES invoice."user" (id),
+    FOREIGN KEY (promo_id) REFERENCES invoice.promo (id)
+  );
 
 ALTER TABLE invoice."transaction"
 ADD FOREIGN KEY("seller_id") REFERENCES invoice."seller"("id")
